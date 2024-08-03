08a652bbbd4570d6ff47f441c064498aee05cd24
Teodor Vidakovic <teodorv22@gmail.com>
2024-08-03 12:16:47 +0200

    feat: implement loss function

---
+++ loss_categorical_cross_entropy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class LossCategoricalCrossEntropy:
+    def __init__(self):
+        self.correct_confidences = None
+
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        if len(y_true.shape) == 1:
+            self.correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            self.correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        negative_log_likelihoods = -np.log(self.correct_confidences)
+        return negative_log_likelihoods
+
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        return np.mean(sample_losses)
