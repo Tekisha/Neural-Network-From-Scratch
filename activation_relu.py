6147fc0296e3abd7f9e12a14c4711fdd360546f7
Teodor Vidakovic <teodorv22@gmail.com>
2024-08-28 14:20:42 +0200

    feat: update relu function

--- activation_relu.py
+++ activation_relu.py
@@ -6,8 +6,16 @@ class ActivationReLU:
     Rectified Linear Activation (ReLU) class.
     Applies the ReLU activation function element-wise.
     """
+
     def __init__(self):
+        self.dinputs = None
+        self.inputs = None
         self.output = None
 
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
