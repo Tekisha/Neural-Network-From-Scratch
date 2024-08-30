cfe9ac0b033277e051db298c8f2eed1229031e2e
Teodor Vidakovic <teodorv22@gmail.com>
2024-08-30 17:27:20 +0200

    feat: implement layer dropout

---
+++ layer_dropout.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class LayerDropout:
+    def __init__(self, rate):
+        self.dinputs = None
+        self.output = None
+        self.binary_mask = None
+        self.inputs = None
+        self.rate = 1 - rate
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape) / self.rate
+        self.output = inputs * self.binary_mask
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
